4eaa27059b9e88682c2a280e45eed2d00f1dc253
Tiago Henrique <tiagohs.dev@gmail.com>
2023-10-06 01:34:54 -0300

    [feat]: define a classe base para as spiders construídas para cada categoria de documento

--- mutuca/spiders/__init__.py
+++ mutuca/spiders/__init__.py
@@ -1,4 +1,28 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+from datetime import datetime
+
+import scrapy
+
+
+class BaseCaruaruParliamentaryAllowanceSpider(scrapy.Spider):
+    def __init__(
+        self, start_date="01/01/2000", end_date=None, row_count="-1", *args, **kwargs
+    ):
+        super(BaseCaruaruParliamentaryAllowanceSpider, self).__init__(*args, **kwargs)
+
+        self.start_date = start_date
+        self.row_count = row_count
+
+        if end_date is not None:
+            try:
+                self.end_date = datetime.strptime(end_date, "%d/%m/%Y").date()
+                self.logger.info(f"Collecting data until {self.end_date}")
+            except ValueError:
+                self.logger.exception(
+                    f"Unable to parse {end_date}. Use %d/%m/%Y date format."
+                )
+                raise
+        elif hasattr(self, "end_date"):
+            self.logger.info(f"Collecting data until {self.end_date}")
+        else:
+            self.end_date = datetime.now().strftime("%d/%m/%Y")
+            self.logger.info("Collecting all data available until now")
