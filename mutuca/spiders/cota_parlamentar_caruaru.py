f1d2b5b04fdde02a861465e8f37a848b5385d62b
Tiago Henrique <tiagohs.dev@gmail.com>
2023-10-06 01:37:13 -0300

    [refactor]: refatora a spider de maneira que ela herde a classe base e produza os itens referentes à cota de alimentação

--- mutuca/spiders/cota_parlamentar_caruaru.py
+++ mutuca/spiders/cota_parlamentar_caruaru.py
@@ -1,37 +1,36 @@
 import scrapy
-from mutuca.items import MutucaItem
-from datetime import datetime
+
+from mutuca.items import ParliamentaryAllowanceItem
+from mutuca.spiders import BaseCaruaruParliamentaryAllowanceSpider
 
 
-class CotaParlamentarCaruaruSpider(scrapy.Spider):
+class ParliamentaryMealAllowanceSpider(BaseCaruaruParliamentaryAllowanceSpider):
     name = "cota_parlamentar"
-
-    custom_settings = {
-        "FEEDS": {
-            "datatest.json": {"format": "json", "ovewrite": True}
-        }
-    }
+    # start_date = "01/01/2000"
+    # end_date = datetime.now().strftime("%d/%m/%Y")
+    # row_count = "-1"
+    custom_settings = {"FEEDS": {"datatest.json": {"format": "json", "ovewrite": True}}}
 
     def start_requests(self):
-        start_date = "01/01/2000"
-        end_date = datetime.now().strftime("%d/%m/%Y")
-        row_count = "-1"
-        url = f"https://transparenciape.com.br/CamaraCaruaru/cotaAtividadeParlamentarClass.php?dataInicial={start_date}&dataFinal={end_date}&rowCount={row_count}"
+        url = f"https://transparenciape.com.br/CamaraCaruaru/cotaAtividadeParlamentarClass.php?dataInicial={self.start_date}&dataFinal={self.end_date}&rowCount={self.row_count}"
 
         yield scrapy.Request(url)
 
     def parse(self, response):
-        rows = response.json()["rows"]
         url_base = "http://transparencia.caruaru.pe.leg.br/sistema/uploads/cotas/"
 
-        for row in rows:
-            url = url_base + row["arquivo"]
-            mutuca_item = MutucaItem()
+        for row in response.json()["rows"]:
+            if "Alimentação" in row["descricao"]:
+                url = url_base + row["arquivo"]
+                publication_date = row["data_publicacao"]
+                description = row["descricao"]
+                file_id = row["arquivo"]
 
-            mutuca_item["file_urls"] = [url]
-            mutuca_item["url"] = url
-            mutuca_item["publication_date"] = row["data_publicacao"]
-            mutuca_item["description"] = row["descricao"]
-            mutuca_item["file_id"] = row["arquivo"]
-
-            yield mutuca_item
+                yield ParliamentaryAllowanceItem(
+                    file_urls=[url],
+                    url=url,
+                    publication_date=publication_date,
+                    description=description,
+                    category="ALIMENTAÇÃO",
+                    file_id=file_id,
+                )
